afb66630769d6fa51b6af3a81887379ddbb2e621
Suraj Singh Chauhan <chauhan.surajsingh556@gmail.com>
2025-05-24 11:19:50 +0530

    exp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Hero from "../components/Hero";
-import FeaturedDestination from "../components/FeaturedDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
-import Testimonial from "../components/Testimonial";
+import FeaturedDestination from "../components/FeaturedDestination";
+import Hero from "../components/Hero";
 import NewsLetter from "../components/Newsletter";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
