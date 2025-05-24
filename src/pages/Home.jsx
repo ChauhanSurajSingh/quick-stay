4b50b80fb9e771041191d883811de43da3fa86f7
Suraj Singh Chauhan <chauhan.surajsingh556@gmail.com>
2025-05-24 11:18:17 +0530

    exp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import FeaturedDestination from "../components/FeaturedDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import Testimonial from "../components/Testimonial";
-// import NewsLetter from "../components/Newsletter";
+import NewsLetter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       <FeaturedDestination />
       <ExclusiveOffers />
       <Testimonial />
-      {/* <NewsLetter /> */}
+      <NewsLetter />
     </>
   );
 };
